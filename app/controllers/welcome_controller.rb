bb1e62b9884d4d65b5ca96144e82dd9afd402166
Marie Denney <mldenne@gmail.com>
2016-07-06 14:17:41 -0400

    Added changes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
+    @title = "Welcome!"
+    @button_options = %w(click_me no_click_me ugh_click_me)
   end
+
+  def button_options
+    if click_me
+      "Click me!"
+      elsi
+
+  end
+
 end
